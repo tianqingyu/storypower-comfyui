c3b2f991ef668e2b990622df3c79e9c4dcb60b27
ruohe <god.yangjun@163.com>
2023-11-04 13:23:18 +0800

    feat: 更新启动文件

--- start_image_2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python main.py --listen --port 39001 --cuda-device 0 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps
--- start_video_2.sh
+++ start_video_2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --listen --port 39003 --cuda-device 2 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps
+python main.py --listen --port 39002 --cuda-device 2 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps
