c8498c23e18c601e3b15363e6ce152dfe5a16552
ruohe <god.yangjun@163.com>
2023-11-11 09:55:05 +0800

    feat: 更新文档和运行文件

--- start_video_2.sh
+++ start_video_2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --listen --port 39002 --cuda-device 2 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps
+python -u main.py --listen --port 39002 --cuda-device 2 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps
