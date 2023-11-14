9bbc25810149b998537eeb476cc00897a656a976
ruohe <god.yangjun@163.com>
2023-11-14 21:33:27 +0800

    feat: 指定input目录（上传文件的保存目录）

--- start_image.sh
+++ start_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python -u main.py --listen --port 39000 --cuda-device 0 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/image.log 2>&1 &
+nohup python -u main.py --listen --port 39000 --cuda-device 0 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/image.log 2>&1 &
--- start_video.sh
+++ start_video.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -e "\n>>> 启动 ComfyUI Video APP1，port=39001"
-nohup python -u main.py --listen --port 39001 --cuda-device 1 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-1.log 2>&1 &
+nohup python -u main.py --listen --port 39001 --cuda-device 1 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-1.log 2>&1 &
 
 echo -e "\n>>> 启动 ComfyUI Video APP2，port=39002"
-nohup python -u main.py --listen --port 39002 --cuda-device 2 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-2.log 2>&1 &
+nohup python -u main.py --listen --port 39002 --cuda-device 2 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-2.log 2>&1 &
 
 echo -e "\n>>> 启动 ComfyUI Video APP3，port=39003"
-nohup python -u main.py --listen --port 39003 --cuda-device 3 --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-3.log 2>&1 &
+nohup python -u main.py --listen --port 39003 --cuda-device 3 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-3.log 2>&1 &
