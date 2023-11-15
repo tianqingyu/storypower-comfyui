d624806d4d23d0776c5743614077c11cfd48b3f5
ruohe <god.yangjun@163.com>
2023-11-15 10:56:38 +0800

    feat: 更新脚本文件

--- start_apps.sh
+++ start_apps.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo -e "\n>>> 启动 ComfyUI Video APP1，port=39001"
+echo -e "\n>>> 启动 ComfyUI Image app, port=39000"
+nohup python -u main.py --listen --port 39000 --cuda-device 0 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/image.log 2>&1 &
+
+echo -e "\n>>> 启动 ComfyUI Video app1，port=39001"
 nohup python -u main.py --listen --port 39001 --cuda-device 1 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-1.log 2>&1 &
 
-echo -e "\n>>> 启动 ComfyUI Video APP2，port=39002"
+echo -e "\n>>> 启动 ComfyUI Video app2, port=39002"
 nohup python -u main.py --listen --port 39002 --cuda-device 2 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-2.log 2>&1 &
 
-echo -e "\n>>> 启动 ComfyUI Video APP3，port=39003"
+echo -e "\n>>> 启动 ComfyUI Video app3, port=39003"
 nohup python -u main.py --listen --port 39003 --cuda-device 3 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/video-3.log 2>&1 &
--- start_image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup python -u main.py --listen --port 39000 --cuda-device 0 --input-directory /home/vantage/apps/output --output-directory /home/vantage/apps/output --temp-directory /home/vantage/apps > /home/vantage/apps/logs/image.log 2>&1 &
