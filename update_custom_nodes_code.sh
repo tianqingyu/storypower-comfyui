c5519b3b782e11e581ba68833344a7345cf94034
ruohe <god.yangjun@163.com>
2023-11-30 15:17:53 +0800

    feat: 更新脚本

--- update_custom_nodes_code.sh
+++ update_custom_nodes_code.sh
@@ -2,28 +2,35 @@
 
 echo -e "\n>>> update Comfy_KepListStuff\n"
 cd custom_nodes/Comfy_KepListStuff/
+git remote -v
 git pull
 
 echo -e "\n>>> update ComfyUI-Advanced-ControlNet\n"
 cd ../ComfyUI-Advanced-ControlNet/
+git remote -v
 git pull
 
 echo -e "\n>>> update ComfyUI-AnimateDiff-Evolved\n"
 cd ../ComfyUI-AnimateDiff-Evolved/
+git remote -v
 git pull
 
 echo -e "\n>>> update ComfyUI_Comfyroll_CustomNodes\n"
 cd ../ComfyUI_Comfyroll_CustomNodes/
+git remote -v
 git pull
 
 echo -e "\n>>> update comfyui_controlnet_aux\n"
 cd ../comfyui_controlnet_aux/
+git remote -v
 git pull
 
 echo -e "\n>>> update ComfyUI_FizzNodes\n"
 cd ../ComfyUI_FizzNodes/
+git remote -v
 git pull
 
 echo -e "\n>>> update ComfyUI-VideoHelperSuite\n"
 cd ../ComfyUI-VideoHelperSuite/
+git remote -v
 git pull
